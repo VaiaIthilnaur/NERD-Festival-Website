c687c3759c4bf040178617259118621c8d674484
Szymon Czartoryski <vaia.ithilnaur.dev@gmail.com>
2020-07-13 17:24:01 +0200

    Main
    All Files at once

---
+++ sendEmail.php
@@ -0,0 +1,24 @@
+﻿<?php
+	$name=$_POST['name'];
+	$visitor_email=$_POST['email'];
+	$subject=$_POST['subject'];
+	$message=$_POST['message'];
+	
+	$email_form='szymon.czartoryski@gmail.com';
+	
+	$email_body="Imię i nazwisko: $name.\n".
+						"Temat: $subject.\n";
+							
+	$to ="nerdfestiwal@gmail.com";
+						
+						
+	$headers="Od: $visitor_email $name\r\n"; 
+	$headers.="Temat: $subject\r\n"; 
+	$headers.="Wiadomość: $message\r\n";
+	
+	mail($to,$email_body,$headers);
+	
+	header("Location: index.html");
+	
+	
+?>
